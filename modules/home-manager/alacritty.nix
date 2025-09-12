ae349f722033b57c5853216e51d5e3c0f650a96c
Fynn Michell-Auli <f.michell-auli@live.de>
2025-09-12 16:40:33 +0200

    alacritty config

--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -8,7 +8,67 @@
   config = lib.mkIf config.alacritty.enable {
     programs.alacritty.enable = true;
     programs.alacritty.theme = "dark_plus";
-    #environment.variables.TERMINAL = "alacritty";
     systemd.user.sessionVariables.TERMINAL = "alacritty";
+
+    programs.alacritty.settings = {
+      font = {
+        size = 10;
+        normal = {
+          family = "Comic Code NerdFont Mono";
+          style = "Regular" ;
+        };
+        italic = {
+          family = "Comic Code NerdFont Mono";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "Comic Code NerdFont Mono";
+          style = "Bold Italic";
+        };
+      };
+
+      window.padding = {
+        x = 2;
+        y = 1;
+      };
+
+      keyboard.bindings = [
+        {
+          action = "IncreaseFontSize";
+          key = "K";
+          mods = "Alt|Shift";
+        }
+        {
+          action = "DecreaseFontSize";
+          key = "J";
+          mods = "Alt|Shift";
+        }
+        {
+          action = "Copy";
+          key = "C";
+          mods = "Alt";
+        }
+        {
+          action = "Paste";
+          key = "V";
+          mods = "Alt";
+        }
+        {
+          action = "ScrollLineUp";
+          key = "K";
+          mods = "Alt";
+        }
+        {
+          action = "ScrollLineDown";
+          key = "J";
+          mods = "Alt";
+        }
+        {
+          action = "ToggleViMode";
+          key = "Escape";
+          mods = "Alt";
+        }
+      ];
+    };
   };
 }
